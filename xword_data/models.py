856608e58e196bf8615b30f7403dcdd466650725
bzhang37 <bzhang37@ncsu.edu>
2021-02-18 13:58:48 -0500

    add models

--- xword_data/models.py
+++ xword_data/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.utils import timezone
+import factory
 
 # Create your models here.
+
+
+class Entry(models.Model):
+    entry_text = models.CharField(max_length=50, required = True, unique = True)
+
+
+class Puzzle(models.Model):
+    title = models.CharField(max_length=255, blank = True)
+    date = models.DateTimeField(default=timezone.now, required = True)
+    byline = models.CharField(max_length=255, required = True)
+    publisher = models.CharField(max_length=255, required = True)
+
+
+class Clue(models.Model):
+    entry = models.ForeignKey(Entry, on_delete=models.CASCADE)
+    puzzle = models.ForeignKey(Puzzle, on_delete=models.CASCADE)
+    clue_text = models.CharField(max_length=512, required = True)
+    theme = models.BooleanField(default=False)
+
+
